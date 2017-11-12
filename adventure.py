86d4e824735723daa0dec18a9b5a46d3277b35f5
Mathilde Barbier <mathilde206@gmail.com>
2017-11-12 14:41:48 +0100

    Add map and location's descriptions

--- adventure.py
+++ adventure.py
@@ -9,18 +9,50 @@ directions = {
 
 position = (0, 0)
 
+
+def print_map(location):
+    """
+    :param location: one of the four possible locations
+    :return: prints the map
+    """
+    north = ["H", "P"]
+    south = ["L", "M"]
+
+    if location == 'house':
+        north[0] = "X"
+    elif location == "lake":
+        south[0] = "X"
+    elif location == "park":
+        north[1] = "X"
+    else:
+        south[1] = "X"
+
+    print str(north) + "\n" + str(south)
+
+
+
+
+def print_description(location):
+    print locations[position]['description']
+
+
+
+
 while True:
-    location = locations[position]
-    print
-    'you are at the %s' % location
+    location = locations[position]['name']
+    print 'You are at the %s' % location
+    print_description(location)
+    print_map(location)
 
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print 'to the %s is a %s' % (k, possible_location)
+            print 'to the %s is a %s' % (k, possible_location['name'])
             valid_directions[k] = possible_position
 
     direction = raw_input('which direction do you want to go?\n')
     position = valid_directions[direction]
+
+
--- data.py
+++ data.py
@@ -1,6 +1,22 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
-}
+    (0, 0): {
+        'name': 'house',
+        'description': "This is where you live",
+        'objects': ['glass', 'blanket', 'book']
+        },
+    (0, 1): {
+        'name': 'lake',
+        'description': "This is where you can swim",
+        'objects': ['bathing suit', 'towel', 'sunglasses']
+         },
+    (1, 0): {
+        'name': 'park',
+        'description': "This is where you can have a picnic",
+        'objects': ['sandwich', 'basket', 'apple']
+         },
+    (1, 1): {
+        'name': 'market',
+        'description': "This is where you can buy food",
+        'objects': ['banana', 'cookies', 'brocoli']
+         },
+    }
